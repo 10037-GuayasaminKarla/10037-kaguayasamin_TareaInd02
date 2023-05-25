5df2351e667228a3f6851b3033f2f750618b4338
Karla Guayasamin <kaguayasamin@espe.edu.ec>
2023-05-25 11:46:29 -0500

    tareaindividual-02

---
+++ tabla/js/main.js
@@ -0,0 +1,7 @@
+function cambiarEstilo() {
+  var titulo = document.getElementById("titulo");
+
+  // Cambiar el color del h1 agregando la clase 'color-change'
+  titulo.classList.toggle("color-change");
+  titulo.classList.toggle("align-text");
+}
